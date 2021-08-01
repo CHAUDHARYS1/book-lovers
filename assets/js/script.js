39c68f4178a6ccdcdd3bd5f482b42571041530ef
Shital Chaudhary <shitalchau10@gmail.com>
2021-08-01 12:41:04 -0500

    generate html component dynamically for book lists

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,101 @@
-var fetchBook = function(searchInput){
-    var apiUrl = "https://www.googleapis.com/books/v1/volumes?q=" + searchInput
-    fetch(apiUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
+var fetchBook = function (searchInput) {
+  var apiUrl = "https://www.googleapis.com/books/v1/volumes?q=" + searchInput;
+  fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
 
-            displaySearchResults(data);
-        })
-}
+      displaySearchResults(data);
+    });
+};
 
 // function to populate search results papge
-var displaySearchResults = function(data) {
-    console.log("title, author, thumbnail, description")
-}
+var displaySearchResults = function (data) {
+  console.log("title, author, thumbnail, description");
+};
 
 $("#search-btn").on("click", function (e) {
-    console.log("here")
-    var searchInput = document.getElementById("search-bar").value;
-    if(searchInput === "") {
-
-    }
-    else {
-        fetchBook(searchInput);
-    }
+  console.log("here");
+  var searchInput = document.getElementById("search-bar").value;
+  if (searchInput === "") {
+  } else {
+    fetchBook(searchInput);
+  }
 });
 
+// generating list of books when searched dynamically.
 
+var displayLists = function () {
+  var individualBookContainerDivEl = document.createElement("div");
+  individualBookContainerDivEl.className = "individual-book-container";
 
+  // get booklist div
+  var bookDivEl = document.createElement("div");
+  bookDivEl.id = "book-list";
+  bookDivEl.className = "uk-grid";
 
+  // get book cover div
+  var thumbnailDivEl = document.createElement("div");
+  thumbnailDivEl.className = "uk-width-auto";
 
+  // get book cover img
+  var thumbnailEl = document.createElement("img");
+  thumbnailEl.id = "book-cover-img";
+  thumbnailEl.setAttribute("src", "assets/images/thumbnail.png");
+  thumbnailEl.setAttribute("alt", "cover photo of book");
+  thumbnailDivEl.appendChild(thumbnailEl);
+  // get book div
+  var bookInfoEl = document.createElement("div");
+  bookInfoEl.className = "uk-width-expand@m";
+  // h4 inside of book div
+  var bookTitleEl = document.createElement("h4");
+  bookTitleEl.id = "book-title";
+  bookTitleEl.classList = "uk-margin-remove-bottom";
+  bookTitleEl.innerHTML = "Title of the book";
+  // <p> for author name inside of book div
+  var authorNameEl = document.createElement("p");
+  authorNameEl.id = "author-name";
+  authorNameEl.classList = "uk-margin-remove-top";
+  authorNameEl.innerHTML = "Author name";
+
+  // <p> for 'description' word inside of book div
+  var descriptionWordEl = document.createElement("p");
+  descriptionWordEl.classList = "uk-margin-remove-button";
+  descriptionWordEl.style.fontWeight = "bold";
+  descriptionWordEl.innerHTML = "Description:";
+
+  // <p> for description for book inside of book div
+  var descriptionEl = document.createElement("p");
+  descriptionEl.id = "book-description";
+  descriptionEl.classList = "uk-margin-remove-top";
+  descriptionEl.innerHTML =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates non quam ad repudiandae ducimus optio, reprehenderit inventore eligendi dolores? Animi blanditiis similique harum obcaecati totam iure, molestiae distinctio! Nostrum, ipsa!";
+
+  // <a> for Learn more inside of book div
+  var learnMoreEl = document.createElement("a");
+  learnMoreEl.id = "learn-more";
+  learnMoreEl.setAttribute("href", "#");
+  learnMoreEl.innerHTML = "Learn more";
+
+  var dividerEl = document.createElement("hr");
+
+  bookInfoEl.append(
+    bookTitleEl,
+    authorNameEl,
+    descriptionWordEl,
+    descriptionEl,
+    learnMoreEl
+  );
+
+  // append 2 child to 1 parent
+  bookDivEl.append(thumbnailDivEl, bookInfoEl);
+  individualBookContainerDivEl.append(bookDivEl, dividerEl);
+
+  var mainContainer = document.querySelector("#main-container");
+  mainContainer.appendChild(individualBookContainerDivEl);
+  console.log(mainContainer);
+};
+
+displayLists();
